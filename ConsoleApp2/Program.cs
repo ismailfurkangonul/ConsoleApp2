8112d4ca2e00a9f49eb05b5a7cd8fb5fc453b761
ismailfurkangonul <ismailfurkangonul2@gmail.com>
2022-07-30 18:14:23 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 20;
+            int sayi2 = 10;
+
+            Generic(sayi1, sayi2);
+            Console.WriteLine(Generic(sayi1, sayi2));
+            Console.ReadLine();
+        }
+        public static string Generic<T, B>(T sayi1, B sayi2)
+        {
+
+           
+            return sayi1.ToString() + sayi2.ToString();
+
+
+       }
+    }
+}
